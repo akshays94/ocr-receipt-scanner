96f1a7f95a219694df7049e2c65f66cb3a29961b
Akshay Suresh <akshaysuresh1994@gmail.com>
2018-11-20 13:30:10 +0530

    ADDED: items in views.py

--- ocr_receipt_scanner/claims/views.py
+++ ocr_receipt_scanner/claims/views.py
@@ -21,7 +21,7 @@ from ocr_receipt_scanner.claims.models import *
 from ocr_receipt_scanner.claims.info_retrieval import *
 from ocr_receipt_scanner.claims.info_retrieval_vars import *
 
-# 13.126.130.131
+#IP Address 13.126.130.131
 
 try:
     from PIL import Image
