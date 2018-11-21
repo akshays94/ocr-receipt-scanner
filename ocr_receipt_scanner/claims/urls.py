2d85777141ed4cbbba7efbc241376793fed8ce00
Akshay Suresh <akshaysuresh1994@gmail.com>
2018-11-21 13:50:45 +0530

    ADDED: success msg on claim submit

--- ocr_receipt_scanner/claims/urls.py
+++ ocr_receipt_scanner/claims/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 import ocr_receipt_scanner.claims.views as views
 
-# from ocr_receipt_scanner.users.views import (
-# 	Home
-# )
 
 app_name = "claims"
 
 urlpatterns = [
     path("", view=views.Home.as_view(), name="home"),
     path("upload-receipt/", view=views.UploadReceipt.as_view(), name="upload-receipt"),
+    path("success/", TemplateView.as_view(template_name="claims/pages/claim-success.html"), name="claim-success")
     # path("~update/", view=user_update_view, name="update"),
     # path("<str:username>/", view=user_detail_view, name="detail"),
 ]
